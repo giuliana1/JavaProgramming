d75633022b6dedb4836664341aa86e0cfbc67444
Giuliana Agreda <giuliana_agreda@hotmail.com>
2022-01-15 16:40:54 -0500

    Commit Java 1/15/22

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -6,6 +6,28 @@ public class BMW extends Car{
         super("BMW", model, year, price, color, miles);
     }
 
+    public void breaksDown(){
+        System.out.println(brand + " "+ model +" is breaking down");
+    }
 
+    public void racing(){
+        System.out.println(brand + " "+ model +" is a racing car");
+    }
+
+
+    public void start(){
+        System.out.println("Call mechanic ");
+
+    }
 
 }
+
+/*
+2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+ */
+
+
+
---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,14 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+
+        Toyota toyota = new Toyota("Camry", 2020, 20000, "Gray", 123456);
+
+        Tesla tesla = new Tesla("Model S", 2022, 94000, "Red", 0);
+
+        BMW bmw= new BMW("X5", 2010, 50000, "green", 0);
+
+    }
+}
